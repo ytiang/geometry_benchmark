8f687f14b100fcefaa978d77598b8017e17294b6
ytiang <1044338070@qq.com>
2020-01-06 19:50:20 +0800

    install release version gtsam

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,7 +2,7 @@
 set -e  # exit on first error
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-
 bash ${CURRENT_DIR}/install_cmake.sh
+bash ${CURRENT_DIR}/install_benchmark.sh
 bash ${CURRENT_DIR}/install_gtsam.sh
 bash ${CURRENT_DIR}/install_mrpt.sh
--- scripts/install_gtsam.sh
+++ scripts/install_gtsam.sh
@@ -15,7 +15,8 @@ install_gtsam() {
 	## install
 	rm -rf build && mkdir build
 	cd build 
-	cmake ..
+
+	cmake -DCMAKE_BUILD_TYPE=Release -DGTSAM_USE_SYSTEM_EIGEN=ON -DGTSAM_WITH_TBB=ON ..
 	make -j$(nproc)
 	sudo make install
 	echo -e "\033[33m install gtsam 4.0.0 successfully \033[0m"
