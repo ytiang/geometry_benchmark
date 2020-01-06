b577dc3027230f500d88c8976aa6dc14c4eec8e3
ytiang <1044338070@qq.com>
2020-01-06 17:15:56 +0800

    install cmake 3.13

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -5,16 +5,12 @@ CMAKE_VERSION=3.13.4
 
 install_cmake() {
 	cd ${SOURCE_DIR}
-	if [ -f "cmake-${CMAKE_VERSION}.tar.gz" ];then
-		echo "Already exist cmake source!"
-	else
-		echo "download cmake source..."
-		wget https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-Linux-x86_64.tar.gz -o cmake-${CMAKE_VERSION}.tar.gz
-	fi
-	rm -rf cmake-${CMAKE_VERSION}
-	tar -xf cmake-${CMAKE_VERSION}.tar.gz
+	rm -rf cmake-${CMAKE_VERSION}*
+	wget https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-Linux-x86_64.tar.gz
+
+	tar -xf cmake-${CMAKE_VERSION}-Linux-x86_64.tar.gz
 	## use version cmake 3.13.4
-	cd cmake-${CMAKE_VERSION}
+	cd cmake-${CMAKE_VERSION}-Linux-x86_64.tar.gz
 	## install
 	./bootstrap && make -j$(nproc) && sudo make install
 	echo -e "\033[33m install cmake 3.13 successfully \033[0m"
