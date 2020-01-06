ce4ec50c4758cc892c12b87b3550ff335990ba12
ytiang <1044338070@qq.com>
2020-01-06 22:16:35 +0800

    install mrpt from source

--- scripts/install_mrpt.sh
+++ scripts/install_mrpt.sh
@@ -2,10 +2,16 @@
 set -e # exit on first error
 SOURCE_DIR="/tmp"
 
+
 install_mrpt() {
-    sudo add-apt-repository ppa:joseluisblancoc/mrpt -y
-    sudo apt-get update
-    sudo apt-get install -y libmrpt-dev mrpt-apps
+	sudo apt install -y build-essential pkg-config libwxgtk3.0-dev libwxgtk3.0-gtk3-dev libopencv-dev libeigen3-dev
+	sudo apt install -y libftdi-dev freeglut3-dev zlib1g-dev libusb-1.0-0-dev libudev-dev libfreenect-dev libdc1394-22-dev libavformat-dev libswscale-dev libassimp-dev libjpeg-dev libsuitesparse-dev libpcap-dev liboctomap-dev
+	cd ${SOURCE_DIR}
+	rm -rf mrpt*
+	git clone https://github.com/MRPT/mrpt.git && cd mrpt && git reset --hard f67d0f8
+	mkdir build && cd build
+	cmake ..
+	make -j$(nproc) && sudo make install
     echo -e "\033[33m install mrpt successfully \033[0m"
 }
 
